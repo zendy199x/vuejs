1171b9242be9c050470f6f8021fd45db41c5a700
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-16 16:35:32 +0700

    mouse listener

--- event/index.js
+++ event/index.js
@@ -2,10 +2,16 @@ new Vue({
 	el: "#app",
 	data: {
 		value: 0,
+		x: 0,
+		y: 0,
 	},
 	methods: {
 		handleIncrease: function () {
 			this.value++
+		},
+		mouseCoordinates: function (event) {
+			this.x = event.clientX
+			this.y = event.clientY
 		},
 	},
 })
