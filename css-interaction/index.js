c179c8f7695d3a42fa26d987d894d32b1ae8be9b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-17 02:30:42 +0700

    css interaction with name variable

--- css-interaction/index.js
+++ css-interaction/index.js
@@ -1,6 +1,15 @@
 var app = new Vue({
 	el: "#app",
 	data: {
-        handleChangeRedColor: false
-    },
+		handleChangeRedColor: false,
+		color: "green",
+	},
+	computed: {
+		divClasses: function () {
+			return {
+				red: this.handleChangeRedColor,
+				blue: !this.handleChangeRedColor,
+			}
+		},
+	},
 })
