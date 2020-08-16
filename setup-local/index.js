836131143dfeb21ef6ba5fa38faee8d69ca281d0
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-16 14:57:25 +0700

    asset data

--- setup-local/index.js
+++ setup-local/index.js
@@ -2,10 +2,14 @@ new Vue({
 	el: "#app",
 	data: {
 		message: "Hello World",
+		body: "Training VueJS",
 	},
 	methods: {
 		sayHello: function () {
 			document.write("Say Hello")
+		},
+		showBody: function () {
+			return this.body
 		},
 	},
 })
