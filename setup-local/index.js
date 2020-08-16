24fdb3e7f76fb8e8ad2b6955c276978d2632f00f
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-16 15:23:10 +0700

    get HTML

--- setup-local/index.js
+++ setup-local/index.js
@@ -4,13 +4,15 @@ new Vue({
 		message: "Hello World",
 		body: "Training VueJS",
 		link: "https://www.facebook.com/zendy199x",
+		testHTML:
+			"<a href='https://www.facebook.com'>Get Facebook</a>",
 	},
 	methods: {
 		sayHello: function () {
 			document.write("Say Hello")
 		},
 		showBody: function () {
-            this.body = "Update body"
+			this.body = "Update body"
 			return this.body
 		},
 	},
