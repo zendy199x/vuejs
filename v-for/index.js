c72b10e04e8a61e1700d6c840f3c0c5606b5761f
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-22 19:21:05 +0700

    for loop

--- v-for/index.js
+++ v-for/index.js
@@ -2,5 +2,10 @@ new Vue({
 	el: "#app",
 	data: {
 		menus: ["Home", "About", "Contact", "FAQs"],
+		students: [
+			{name: "Zendy", age: 27, class: "IT"},
+			{name: "Jurry", age: 28, class: "Finance"},
+			{name: "Cheng", age: 30, class: "IT"},
+		],
 	},
 })
