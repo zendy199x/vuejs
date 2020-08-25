e3f20d31cfd81211e50a9a4b2621d71172358c78
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-08-26 03:39:05 +0700

    setting change hp

--- first-project/app.js
+++ first-project/app.js
@@ -11,5 +11,39 @@ new Vue({
 			this.playerHealth = 100
 			this.monsterHealth = 100
 		},
+		attack: function () {
+			//Monster
+			if (this.monsterHealth <= 0) {
+				alert("Player Win")
+				this.gameIsRunning = false
+				return
+			}
+
+			var maxDamage = 10
+			var minDamage = 4
+			var damage = Math.max(
+				Math.floor(Math.random() * maxDamage) + 1,
+				minDamage
+			)
+			this.monsterHealth -= damage
+
+			//Player
+			if (this.playerHealth <= 0) {
+				alert("Monster Win")
+				this.gameIsRunning = false
+				return
+			}
+
+			var maxDamage = 12
+			var minDamage = 5
+			var damage = Math.max(
+				Math.floor(Math.random() * maxDamage) + 1,
+				minDamage
+			)
+			this.playerHealth -= damage
+		},
+		specialAttack: function () {},
+		heal: function () {},
+		giveUp: function () {},
 	},
 })
