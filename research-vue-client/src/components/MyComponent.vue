725a5ccd30f4a132a11d66a66d0cdbb5370ce0f3
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-16 23:37:02 +0700

    2 type use data

--- research-vue-client/src/components/MyComponent.vue
+++ research-vue-client/src/components/MyComponent.vue
@@ -1,14 +1,26 @@
 <template>
-	<h1>Hello {{ test }}</h1>
+	<div id="my-component">
+		<h1>Hello {{ test }}</h1>
+		<button @click="changeTest">Click</button>
+	</div>
 </template>
 
 <script>
+// var data = { // change all
+// 	test: "VueJS",
+// }
 export default {
-	name: "MyComponent",
+	name: "my-component",
 	data() {
 		return {
-			test: "VueJS",
+			test: "VueJS", // change unique
 		}
+	},
+	methods: {
+		changeTest: function() {
+			this.test = "Change Test"
+			console.log(this)
+		},
 	},
 }
 </script>
