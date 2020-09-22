7ff901b1d26be60dcaa435b57df0f8ed4973ffbd
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-23 02:10:23 +0700

    use one component in vuejs cli

--- create-component/src/Contact.vue
+++ create-component/src/Contact.vue
@@ -1,11 +1,18 @@
 <template>
 	<div>
 		<h1>From Contact Page</h1>
+    <datacontact-component></datacontact-component>
 	</div>
 </template>
 
 <script>
-export default {}
+import DataContact from "./DataContact.vue"
+
+export default {
+	components: {
+		"datacontact-component": DataContact,
+	},
+}
 </script>
 
 <style></style>
---
+++ create-component/src/DataContact.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<p>My address: {{ address }}</p>
+		<hr />
+		<button @click="changePhone">Show My Phone</button>
+	</div>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			address: "Hai Duong",
+		}
+	},
+	methods: {
+		changePhone() {
+			this.address = "0388171781"
+		},
+	},
+}
+</script>
+
+<style></style>
