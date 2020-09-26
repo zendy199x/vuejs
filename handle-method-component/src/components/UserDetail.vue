a4939ddbd441f084ab6bc22d3d9996b9f58018a2
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-26 23:48:07 +0700

    using props

--- handle-method-component/src/components/UserDetail.vue
+++ handle-method-component/src/components/UserDetail.vue
@@ -2,13 +2,21 @@
 	<div class="component">
 		<h3>Infomation User Details</h3>
 		<p>List Details</p>
-		<p>My Name: {{ customName }}</p>
+		<p>My Name: {{ reverseName() }}</p>
 	</div>
 </template>
 
 <script>
 export default {
 	props: ["customName"],
+	methods: {
+		reverseName() {
+			return this.customName
+				.split("")
+				.reverse()
+				.join("")
+		},
+	},
 }
 </script>
 
