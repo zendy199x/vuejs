dd8c748d8f21650fb18ff9c14435ef7836eb02c4
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-26 18:18:10 +0700

    structure component

---
+++ structure-component/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="container">
+		<app-header></app-header>
+		<hr />
+		<div class="row">
+			<servers></servers>
+			<app-server-details></app-server-details>
+		</div>
+		<hr />
+		<app-footer></app-footer>
+	</div>
+</template>
+
+<script>
+import Header from "./components/Shared/Header.vue"
+import Footer from "./components/Shared/Footer.vue"
+import Servers from "./components/Servers/Servers.vue"
+import ServerDetails from "./components/Servers/ServerDetails.vue"
+
+export default {
+	components: {
+		appHeader: Header, //"app-header": Header
+		Servers, //"servers": Servers
+		"app-server-details": ServerDetails,
+		"app-footer": Footer,
+	},
+}
+</script>
+
+<style></style>
---
+++ structure-component/src/components/Servers/Servers.vue
@@ -0,0 +1,7 @@
+<template>
+	<div class="col-xs-12 col-sm-6">
+		<ul class="list-group-item" v-for="value in 5" :key="value">
+			<li>Server #{{ value }}</li>
+		</ul>
+	</div>
+</template>
