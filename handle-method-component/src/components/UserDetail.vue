9d54cf63afedb915c03c53b23439f709240b269d
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-26 19:53:34 +0700

    props in VueJS

--- handle-method-component/src/components/UserDetail.vue
+++ handle-method-component/src/components/UserDetail.vue
@@ -2,11 +2,15 @@
 	<div class="component">
 		<h3>Infomation User Details</h3>
 		<p>List Details</p>
-		<p>My name: {{ name }}</p>
+		<p>My Name: {{ customName }}</p>
 	</div>
 </template>
 
-<script></script>
+<script>
+export default {
+	props: ["customName"],
+}
+</script>
 
 <style scoped>
 div {
