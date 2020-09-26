b2d22d6dec794550a7bfe25ae3c750d73ba73aec
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-26 19:34:14 +0700

    tmeplate component method

---
+++ handle-method-component/src/components/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="component">
+		<h3>Infomation User Details</h3>
+		<p>List Details</p>
+		<p>My name: {{ name }}</p>
+	</div>
+</template>
+
+<script></script>
+
+<style scoped>
+div {
+	background-color: lightcoral;
+}
+</style>
