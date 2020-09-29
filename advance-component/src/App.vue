03049b80533caa21e1c313c17870f6e5e8737e10
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-09-29 23:48:08 +0700

    setup module basic

---
+++ advance-component/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <Card />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ advance-component/src/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+	<div>
+		<h2>Car</h2>
+		<p>This is my car !</p>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+div {
+  width: 100%;
+	border: 1px solid #ccc;
+	box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.6);
+	padding: 30px;
+	margin: 20px auto;
+	text-align: center;
+}
+</style>
