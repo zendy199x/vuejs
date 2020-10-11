ec60038c9a10cfb0b99dc226bdb4a31740d3a14b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-10-12 03:03:18 +0700

    destroyed when convert component

--- convert-component/src/components/Contact.vue
+++ convert-component/src/components/Contact.vue
@@ -1,9 +1,22 @@
 <template>
 	<div>
 		<h1>This is Contact Page</h1>
+		<button @click="counter++">Increase Value</button>
+		<p>{{ counter }}</p>
 	</div>
 </template>
 
-<script></script>
+<script>
+export default {
+	data: function() {
+		return {
+			counter: 0,
+		}
+	},
+	destroyed() {
+		console.log("Destroyed")
+	},
+}
+</script>
 
 <style scoped></style>
