3a5ed7f31ac0e6ea9991ea769b2006b53f75876b
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-10-13 01:06:13 +0700

    init mini project

---
+++ mini-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+	<div id="app"></div>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			items: ["Zendy"],
+			maxItems: 10,
+		}
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
