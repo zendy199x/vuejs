877bf6c4a039f7c772e3b5bd620fa07c6ef34021
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-10-13 01:42:23 +0700

    create new item

---
+++ mini-project/src/components/NewItem.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="row">
+		<form>
+			<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+				<label for="">You can saying...</label>
+				<textarea
+					class="form-group"
+					v-model="item"
+					name=""
+					id=""
+					cols="30"
+					row="10"
+				></textarea>
+			</div>
+
+			<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
+				<button class="btn btn-success" @click.prevent="createNew">Add</button>
+			</div>
+		</form>
+	</div>
+</template>
+
+<script>
+export default {
+	data: function() {
+		return {
+			item: "",
+		}
+	},
+}
+</script>
+
+<style></style>
