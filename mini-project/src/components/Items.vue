ce5605b9e20c6633563e9bf28f2fa8e1dcf6ae30
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-10-13 01:15:03 +0700

    create core mini project

---
+++ mini-project/src/components/Items.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="col-sm-6 col-md-4 col-lg-3">
+		<div class="card">
+			<div class="card-body">
+				<slot></slot>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.card-body {
+	font-family: "Arizonia", cursive;
+	font-size: 24px;
+	color: #6e6e6e;
+}
+
+.item {
+	cursor: pointer;
+}
+
+.item:hover {
+	background-color: $ffe2e2;
+}
+</style>
---
+++ mini-project/src/components/ItemsLayout.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<span>{{ items }}</span>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ["items"],
+}
+</script>
+
+<style lang="scss"></style>
