44cbd6b57e3ddc2efccb9a27fed193bdce406ca3
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-10-28 01:29:15 +0700

    register local a custom directives

--- directives/src/App.vue
+++ directives/src/App.vue
@@ -11,13 +11,34 @@
 		<div class="row">
 			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
 				<h1>Custom Directives</h1>
-				<p v-custom:background.delayed="'green'">Color style by directives</p>
+				<p v-custom:background.delayed="'green'">Color style by directives Global</p> 
+				<p v-local-custom:background.delayed="'green'">Color style by directives Local</p>
 			</div>
 		</div>
 	</div>
 </template>
 
-<script></script>
+<script>
+export default {
+	directives: {
+		"local-custom": {
+			bind(el, binding) {
+				let delay = 0
+				if (binding.modifiers["delayed"]) {
+					delay = 3000
+				}
+				setTimeout(() => {
+					if (binding.arg === "background") {
+						el.style.backgroundColor = binding.value
+					} else {
+						el.style.color = binding.value
+					}
+				}, delay)
+			},
+		},
+	},
+}
+</script>
 
 <style>
 #app {
