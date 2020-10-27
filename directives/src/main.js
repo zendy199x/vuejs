f8db978b6c15fcee2a0efb30cafa952554638677
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-10-28 00:59:24 +0700

    create directives simple

--- directives/src/main.js
+++ directives/src/main.js
@@ -3,7 +3,11 @@ import App from "./App.vue"
 
 Vue.config.productionTip = false
 
-Vue.directive("highlight", {})
+Vue.directive("highlight", {
+	bind(el           ) {
+		el.style.backgroundColor = "yellow"
+	},
+})
 
 new Vue({
 	render: (h) => h(App),
