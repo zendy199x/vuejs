509848cddb0c976823c85c6145c5c3e637b20aff
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-11-30 02:02:19 +0700

    simple transition

---
+++ trasitions-animation/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <div clas="row">
+      <div
+        class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 text-center"
+      >
+        <h1>Animations</h1>
+        <hr />
+        <button class="btn btn-block btn-success" @click="show = !show">
+          Show Notification
+        </button>
+        <br />
+        <br />
+
+        <transition>
+          <div class="alert alert-success" v-if="show">
+            This is something notification
+          </div>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+    };
+  },
+};
+</script>
+
+<style></style>
