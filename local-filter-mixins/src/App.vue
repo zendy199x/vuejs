8ff1292b0a432b4596693395867c9cc04d5ca7c9
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-11-29 23:22:41 +0700

    create a global filter

--- local-filter-mixins/src/App.vue
+++ local-filter-mixins/src/App.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
       <h1>Filter & Mixins</h1>
-      <p>{{ text | toLowerCase }}</p>
+      <p>{{ text | toLowerCase | to-uppercase }}</p>
     </div>
   </div>
 </template>
