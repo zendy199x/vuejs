21f73b977d898605e7c2c944f792265b88f507d7
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-11-29 23:14:44 +0700

    create a local filter

---
+++ local-filter-mixins/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+      <h1>Filter & Mixins</h1>
+      <p>{{ text | toLowerCase }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hello everywhere, VueJS",
+    };
+  },
+  filters: {
+    toLowerCase(text) {
+      return text.toLowerCase();
+    },
+  },
+};
+</script>
+
+<style></style>
