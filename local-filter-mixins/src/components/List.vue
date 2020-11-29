8249237c99c392416e71b738f03518ddec335f3a
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-11-30 00:39:11 +0700

    learn about mixins

--- local-filter-mixins/src/components/List.vue
+++ local-filter-mixins/src/components/List.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="container">
-    <div clas="row">
-      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-        <h1>Filter & Mixins</h1>
-        <p>{{ text | toLowerCase | toUpperCase }}</p>
-      </div>
-    </div>
+  <div class="row">
     <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
-      <h3>Filter Products</h3>
+      <h3>Filter Products From List Component</h3>
       <input type="text" class="form-control mb-md-3" v-model="filterProduct" />
       <div class="card">
         <div class="card-body">
@@ -19,33 +13,21 @@
         </div>
       </div>
     </div>
-    <List></List>
   </div>
 </template>
 
 <script>
-import List from "./components/List.vue";
-
 export default {
-  components: {
-    List,
-  },
   data() {
     return {
-      text: "Hello everywhere, VueJS",
       products: ["Apple", "Samsung", "HTC", "Nokia", "Huawei", "Oppo"],
       filterProduct: "",
     };
   },
-  filters: {
-    toLowerCase(text) {
-      return text.toLowerCase();
-    },
-  },
   computed: {
     filteredProduct() {
       return this.products.filter((el) =>
-        el.toLowerCase().match(this.filterProduct.toLowerCase())
+        el.toLowerCase().includes(this.filterProduct.toLowerCase())
       ); // can use includes prototype
     },
   },
