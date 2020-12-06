fc1ed60d9f8bfd21fe3d1d85128deacfacf4c5dc
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-12-07 00:20:36 +0700

    firebase

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -1,19 +1,42 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="row">
+      <div
+        class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 text-left"
+      >
+        <h1 class="text-center">Vue Resource</h1>
+        <div class="form-group">
+          <input class="form-control" type="text" v-model="user.username" />
+        </div>
+
+        <div class="form-group">
+          <input class="form-control" type="text" v-model="user.email" />
+        </div>
+
+        <button class="btn btn-block btn-success" type="submit" @click="submit">
+          Submit
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      user: {
+        username: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    submit() {
+      console.log("Submit", this.user);
+    },
+  },
+};
 </script>
 
 <style>
