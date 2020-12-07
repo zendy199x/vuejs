b81549ee159de2595a12ae60f7fd9f8fd6c26db5
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-12-08 00:55:40 +0700

    add vue-router

---
+++ vue-router/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div
+        class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 text-left"
+      >
+        <h1 class="text-center">
+          Vue Routing
+        </h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
