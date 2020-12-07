82070650477e8106c5349e00af7d9c34efe5c4ea
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-12-07 23:14:10 +0700

    Interceptor with request

--- vue-resource/src/main.js
+++ vue-resource/src/main.js
@@ -5,7 +5,13 @@ import VueResource from "vue-resource";
 Vue.config.productionTip = false;
 
 Vue.use(VueResource);
-Vue.http.options.root = "https://vue-resource-28a2e-default-rtdb.firebaseio.com/data.json"
+Vue.http.options.root =
+  "https://vue-resource-28a2e-default-rtdb.firebaseio.com/data.json";
+Vue.http.interceptors.push((request, next) => {
+  console.log(request);
+  if (request.method == "POST") request.method = "PUT";
+  next();
+});
 
 new Vue({
   render: (h) => h(App),
