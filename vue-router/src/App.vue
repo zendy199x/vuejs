5d001d28fb93d482c8e2e71a9f086076bc08e470
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-12-13 01:36:27 +0700

    transition router

--- vue-router/src/App.vue
+++ vue-router/src/App.vue
@@ -3,7 +3,9 @@
     <!-- <Header /> -->
     <router-view name="header" />
     <div class="container">
-      <router-view />
+      <transition name="slide">
+        <router-view />
+      </transition>
     </div>
   </div>
 </template>
@@ -22,11 +24,31 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir,  Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.slide-leave-active {
+  transition: opacity 1s ease;
+  opacity: 0;
+  animation: slide-out 1s ease-out forwards;
+}
+
+.slide-leave {
+  opacity: 1;
+  transform: translateX(0);
+}
+
+@keyframes slide-out {
+  0% {
+    transform: translateX(0);
+  }
+  100% {
+    transform: translateX(30px);
+  }
 }
 </style>
