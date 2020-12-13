b60960c7f5d53b79a75a33af598ac18331d648c9
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2020-12-13 14:02:30 +0700

    scrollBehavior

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -9,6 +9,16 @@ Vue.use(VueRouter);
 const router = new VueRouter({
   mode: "history",
   routes,
+  // scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(to) {
+    // if (savedPosition) {
+    //   return savedPosition;
+    // }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    // return { x: 0, y: 700 };
+  },
 });
 
 new Vue({
